fa09a47126a82c622ef029e7502051374c322e8b
millemme <59627262+millemme@users.noreply.github.com>
2021-03-06 21:20:42 -0800

    Add files via upload
    first test typo

--- test_hw7_leap.py
+++ test_hw7_leap.py
@@ -4,7 +4,7 @@ import hw7_leap
 class Test_hw7_fizz(unittest.TestCase):
     def test_leap(self):
         result = hw7_leap.leap_year(1)
-        self.assertEqual(result, true)
+        self.assertEqual(result, "no")
    
 
 if __name__ == '__main__':
