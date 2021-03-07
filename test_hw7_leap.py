c769a53dfede37c850b3f657fbb4ad6013b714a4
millemme <59627262+millemme@users.noreply.github.com>
2021-03-06 21:34:54 -0800

    Add files via upload
    Added a test for year 400, should output yes

--- test_hw7_leap.py
+++ test_hw7_leap.py
@@ -14,6 +14,10 @@ class Test_hw7_fizz(unittest.TestCase):
     def test_leap(self):
         result = hw7_leap.leap_year(200)
         self.assertEqual(result, "no")
+    def test_leap(self):
+        result = hw7_leap.leap_year(400)
+        self.assertEqual(result, "yes")
+
    
 
 if __name__ == '__main__':
