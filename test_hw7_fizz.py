220fef67f169382a6b93d01c138251468e306580
millemme <59627262+millemme@users.noreply.github.com>
2021-03-06 19:47:47 -0800

    Add files via upload
    Fixed an a quick file name typo

--- test_hw7_fizz.py
+++ test_hw7_fizz.py
@@ -3,7 +3,7 @@ import hw7_fizz
 
 class Test_hw7_fizz(unittest.TestCase):
     def test_fizz(self):
-        result = hwy_fizz.fizz_buzz(1)
+        result = hw7_fizz.fizz_buzz(1)
         self.assertEqual(result, 1)
 
 if __name__ == '__main__':
