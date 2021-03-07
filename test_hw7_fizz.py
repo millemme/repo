7db6e8ea0457aee2d01ec29b3745590457c89d01
millemme <59627262+millemme@users.noreply.github.com>
2021-03-06 20:06:20 -0800

    Add files via upload
    Added a test to make sure the program will print FizzBuzz when it is fed 30 as input

--- test_hw7_fizz.py
+++ test_hw7_fizz.py
@@ -11,6 +11,9 @@ class Test_hw7_fizz(unittest.TestCase):
     def test_fizz(self):
         result = hw7_fizz.fizz_buzz(5)
         self.assertEqual(result, "Buzz")
+    def test_fizz(self):
+        result = hw7_fizz.fizz_buzz(30)
+        self.assertEqual(result, "FizzBuzz")
 
 if __name__ == '__main__':
     unittest.main()
