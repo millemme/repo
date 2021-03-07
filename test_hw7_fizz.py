7ab5c04353126316ec1a333b9318ee90fb357f38
millemme <59627262+millemme@users.noreply.github.com>
2021-03-06 19:50:59 -0800

    Add files via upload
    Added a test to test for the program to output Fizz for the number 3

--- test_hw7_fizz.py
+++ test_hw7_fizz.py
@@ -5,6 +5,9 @@ class Test_hw7_fizz(unittest.TestCase):
     def test_fizz(self):
         result = hw7_fizz.fizz_buzz(1)
         self.assertEqual(result, 1)
+    def test_fizz(self):
+        result = hw7_fizz.fizz_buzz(3)
+        self.assertEqual(result, Fizz)
 
 if __name__ == '__main__':
     unittest.main()
